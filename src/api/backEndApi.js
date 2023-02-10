3dd98986bfc0b6afb448ecda5304c40bd8085e00
khabLexander <mantillagka@hotmail.com>
2023-02-10 15:47:46 -0500

    De vuelta

--- src/api/backEndApi.js
+++ src/api/backEndApi.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 //connectar al customer-microservice
 export const backEndApi = axios.create({
-    baseURL: `https://krugercell-service.sistemaagil.net:8082/api/`,
+    baseURL: `http://krugercell-service.sistemaagil.net:8082/api/`,
 }) 
