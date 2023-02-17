11e0d6a2e8290ad2c3945dd9200ae9a67bf1f9e9
khabLexander <mantillagka@hotmail.com>
2023-02-17 12:33:05 -0500

    Json web token expirate validation

---
+++ src/components/AuthVerify/AuthVerify.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+
+const parseJwt = (token) => {
+    try {
+      return JSON.parse(atob(token.split(".")[1]));
+    } catch (e) {
+      return null;
+    }
+};
+
+export const AuthVerify = (props) => {
+    
+    let location = useLocation();
+
+    useEffect(() => {
+        const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    
+        if (currentUser) {
+          const decodedJwt = parseJwt(currentUser.token);
+          console.log(decodedJwt)
+    
+          if (decodedJwt.exp * 1000 < Date.now()) {
+            props.logOut();
+          }
+        }
+      }, [location, props]);
+    
+    return ;
+}
