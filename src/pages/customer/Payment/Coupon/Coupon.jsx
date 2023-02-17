1f321adfa0b4f4408ebb89bf2e2df187391119ab
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-02-17 00:42:00 -0500

    test deploy

--- src/pages/customer/Payment/Coupon/Coupon.jsx
+++ src/pages/customer/Payment/Coupon/Coupon.jsx
@@ -4,68 +4,77 @@ import { validarCupones } from "../../../../helpers/coupons/validarCupones";
 import "./coupon.scss";
 import InformativeMessage from "./informativeMessage/InformativeMessage";
 
-const Coupon = ({onCouponActivated}) => {
+const Coupon = ({ onCouponActivated }) => {
+  const [cuponStr, setCuponStr] = useState("");
+  const [showCupon, setShow] = useState(false);
 
-    const [cuponStr,setCuponStr]=useState("");
-    const[showCupon,setShow]=useState(false);
+  const [messageState, setMessageState] = useState(false);
+  const [message, setMessage] = useState("");
 
-    const [messageState,setMessageState]=useState(false);
-    const[message,setMessage]=useState("");
+  const btnRef = useRef(null);
+  const inputRef = useRef(null);
 
-    const btnRef=useRef(null);
-    const inputRef=useRef(null);
-       
+  function handleOnchange(e) {
+    setCuponStr(e.target.value);
+  }
+  /**
+   * este metod se invoka la presionar validar coupon
+   * para poder determinar si el coupon esta reservado o usado
+   */
+  async function validarCupon() {
+    const resp = await validarCupones(cuponStr);
 
-    function handleOnchange(e){
-        setCuponStr(e.target.value);
-        
+    if (resp && resp.status !== "USED") {
+      //caso el coupon es valido y no esta usado
+      setMessageState(true);
+      setMessage("El cupón ha sido activado!");
+      onCouponActivated(resp);
+      btnRef.current.style.display = "none";
+      inputRef.current.disabled = true;
+      inputRef.current.style.color = "#A1FF69";
+
+      setShow(true);
+    } else {
+      setMessageState(false);
+      setMessage(
+        !resp
+          ? "Cupon is not valid!"
+          : `Cupon is already ${resp.status.toLowerCase()}!`
+      );
+      setShow(true);
+      inputRef.current.style.color = "#ef5353";
+      setTimeout(() => {
+        inputRef.current.style.color = "white";
+
+        setShow(false);
+      }, 1500);
     }
-    /**
-     * este metod se invoka la presionar validar coupon
-     * para poder determinar si el coupon esta reservado o usado
-     */
-async function validarCupon(){
-    
-    const resp=await validarCupones(cuponStr); 
+  }
 
-        if(resp && resp.status !== "USED" ){
-            
-            //caso el coupon es valido y no esta usado
-            setMessageState(true);
-            setMessage("Cupon has been activated!");
-            onCouponActivated(resp);
-            btnRef.current.style.display="none";
-            inputRef.current.disabled=true;
-            inputRef.current.style.color="#A1FF69";
+  return (
+    <div>
+      <div className="coupon-container">
+        <h4>Tienes cupón? Ingrésalo aqui!</h4>
+        <input
+          type="text"
+          placeholder="Ingresa un cupón"
+          value={cuponStr}
+          onChange={handleOnchange}
+          ref={inputRef}
+        />
+        <button onClick={validarCupon} ref={btnRef} className="validar-btn">
+          Validar
+        </button>
+      </div>
+      <div className="mesasge-container">
+        <InformativeMessage
+          message={message}
+          state={messageState}
+          show={showCupon}
+        />
+      </div>
+    </div>
+  );
+};
 
-            setShow(true);  
-        }else{
-            setMessageState(false);
-            setMessage(!resp? "Cupon is not valid!": `Cupon is already ${resp.status.toLowerCase()}!`);
-            setShow(true);  
-            inputRef.current.style.color="#ef5353";
-            setTimeout(()=>{
-                inputRef.current.style.color="white";
-                
-                setShow(false);  
-            },1500)
-        }
-}
-
-    return ( 
-        <div>
-        <div className="coupon-container">
-            <h4>Tienes cupon? ingresalo aqui!</h4>
-            <input type="text"  placeholder="Ingresa un coupon" value={cuponStr} onChange={handleOnchange} ref={inputRef}/>
-            <button onClick={validarCupon} ref={btnRef} className="validar-btn">Validar</button>
-            
-            
-        </div>
-        <div className="mesasge-container">
-            <InformativeMessage message={message} state={messageState} show={showCupon}/>
-        </div>
-        </div>
-     );
-}
- 
-export default  Coupon ;
+export default Coupon;
