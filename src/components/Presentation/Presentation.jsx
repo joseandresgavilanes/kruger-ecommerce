e3df047a0e72b6ebca123b0469ca8b89a837aae2
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-02-18 00:02:14 -0500

    final commit

---
+++ src/components/Presentation/Presentation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Presentation.scss";
+
+const Presentation = () => {
+  return (
+    <div className="pixel_container">
+      <div className="pixel_main_presentation">
+        <div className="presentation"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Presentation;
